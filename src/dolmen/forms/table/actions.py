cdad66f17f316c6182e78a246f7c49fc0ddbb90d
Alex Garel <alex.garel@gmail.com>
2012-05-11 19:03:21 +0200

    action get a single arg like classic actions, validate then run actions, bubble up line errors

--- src/dolmen/forms/table/actions.py
+++ src/dolmen/forms/table/actions.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from dolmen.forms.base import _
-from dolmen.forms.base.markers import NO_VALUE, SUCCESS, FAILURE, NOTHING_DONE
+from dolmen.forms.base.markers import (
+    NO_VALUE, SUCCESS, FAILURE, NOTHING_DONE, getValue)
 from dolmen.forms.base.actions import Actions
 from dolmen.forms.base.errors import Error
 from dolmen.forms.base.interfaces import IWidgetExtractor, ActionError
@@ -9,8 +10,33 @@ from dolmen.forms.table import interfaces
 from zope.component import getMultiAdapter
 
 
+def verify_post(action, form, request):
+    isPostOnly = getValue(action, 'postOnly', form)
+    if isPostOnly and request.method != 'POST':
+        form.errors.append(
+            Error('This form was not submitted properly.',
+                  form.prefix))
+        return False
+    else:
+        return True
+
+
+def merge_errors(line, form):
+    """Merge global line errors to form errors"""
+    target = form.errors
+    add_marker = False
+    for error in line.errors:
+        if error.identifier == line.prefix:
+            target.append(Error(error.title, form.prefix))
+            add_marker = True
+    if add_marker:
+        line.errors.append(Error(' ', 'select'))
+
+
 class TableActions(Actions):
     """Actions that can be applied on a table.
+
+    Actions are applied for each selected table line.
     """
 
     def process(self, form, request):
@@ -23,22 +49,43 @@ class TableActions(Actions):
                 (action, form, request), IWidgetExtractor)
             value, error = extractor.extract()
             if value is not NO_VALUE:
+                if not verify_post(action, form, request):
+                    return action, FAILURE
                 if not ready:
                     form.updateLines(mark_selected=True)
                     ready = True
+                # first validate
+                got_error = False
                 for line in form.lines:
                     if not line.selected:
                         continue
                     one_selected = True
                     try:
-                        if action.validate(line):
-                            content = line.getContentData().getContent()
-                            action(form, content, line)
+                        got_error |=  not action.validate(line)
                     except ActionError, e:
-                        line.errors.append(Error(e.args[0], line.prefix))
+                        got_error = True
+                    if line.errors:
+                        merge_errors(line, form)
+
                 if not one_selected:
                     form.errors.append(
                         Error(_(u"You didn't select any item!"), None))
+                    got_error = True
+
+                if got_error:
                     return action, FAILURE
-                return action, SUCCESS
+
+                # then compute
+                for line in form.lines:
+                    if not line.selected:
+                        continue
+                    try:
+                        status = action(line)
+                        got_error |= status != SUCCESS
+                    except ActionError, e:
+                        form.errors.append(Error(e.args[0], line.prefix))
+                        got_error = True
+                    if line.errors:
+                        merge_errors(line, form)
+                return action, SUCCESS if not got_error else FAILURE
         return None, NOTHING_DONE
